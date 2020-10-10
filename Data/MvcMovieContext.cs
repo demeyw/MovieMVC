cce117b49dd32deedc5b5c81ab4ff7457e5d2342
Wim Demey <wdemey@ctg.com>
2020-10-11 00:20:29 +0200

    Add project files.

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MySql.Data.Entity;
+using System;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        //public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+         : base(options)
+        //: base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+        private class DbConfigurationTypeAttribute : Attribute
+        {
+        }
+    }
+}
